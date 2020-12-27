8e624e36e1892a2229b5ada2a463f52919e1ec3e
Manuel Coppotelli <manuelcop@gmail.com>
2020-12-27 18:07:45 +0100

    Reactoring Image component

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,19 +1,21 @@
 <script>
-  export let alt = ""
-  export let width = null
+  export let alt = ''
   export let height = null
-  export let src = ""
-  export let srcset = ""
-  export let srcsetWebp = ""
-  export let sizes = "(max-width: 1000px) 100vw, 1000px"
+  export let ratio = '100%'
+  export let sizes = '(max-width: 1000px) 100vw, 1000px'
+  export let src = ''
+  export let srcset = ''
+  export let srcsetWebp = ''
+  export let width = null
   export let wrapperClass = null
 
-  let className = ""
+  let className = ''
   export { className as class }
+
 </script>
 
 
-<picture class={wrapperClass}>
+<picture class={wrapperClass} style="padding-bottom:{ratio};">
   <source type="image/webp" srcset="{srcsetWebp}" {sizes} />
   <source {srcset} {sizes} />
   <img
